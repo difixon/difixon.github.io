ad8386ef0099ff11df011ad35dfe2169f6388649
Alexander Batarin <31259048+difixon@users.noreply.github.com>
2017-09-07 01:22:18 +0300

    correctives

--- js/custom.js
+++ js/custom.js
@@ -17,6 +17,12 @@ $( function() {
             $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
           }
         });
+		$('.countdown.styled_rus').countdown({
+          date: endDate,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>дней</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>часов</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>минут</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>секунд</span></div>");
+          }
+        });
 
         $('.countdown.callback').countdown({
           date: +(new Date) + 10000,
